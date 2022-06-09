10ebf0b69c1d509865ab9512a20fe1cc702faa4b
MartC53 <82014002+MartC53@users.noreply.github.com>
2022-06-09 11:57:13 -0700

    smoke test 2

--- qiaml/tests_autocrop/test_autocrop.py
+++ qiaml/tests_autocrop/test_autocrop.py
@@ -12,7 +12,7 @@ from qiaml.get_data import get_data_array
 This test file tests the TestCrop function and get_data_array function. Please run it under QIAML/qiaml folder.
 """
 
-#class TestCrop(unittest.TestCase):
+class TestCrop(unittest.TestCase):
  #   def test_smoke(self):
   #      crop('test_image2.jpg')
    #     return
